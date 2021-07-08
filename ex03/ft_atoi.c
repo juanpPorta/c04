ea10b26fa06db47c7825ea2c45618183de72f491
Juan Porta <jporta@c3r14s1.42madrid.com>
2021-07-08 15:24:46 +0200

    ultima subida

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,82 +6,34 @@
 /*   By: jporta <jporta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 03:53:36 by jporta            #+#    #+#             */
-/*   Updated: 2021/07/06 10:12:49 by jporta           ###   ########.fr       */
+/*   Updated: 2021/07/08 14:47:23 by jporta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 #include <stdio.h>
 
-int	ft_signo(char *signo)
-{
-	int	i;
-	int	a;
-	int	w;
-
-	i = 0;
-	a = 0;
-	while (signo[i] != '\0')
-	{
-		while (signo[i] == '-' || signo[i] == '+')
-		{
-			while (signo[i] == 45)
-			{
-				a++;
-				i++;
-			}
-			i++;
-		}
-		i++;
-	}
-	w = a % 2;
-	if (w == 0)
-	{
-		return ('+');
-	}
-	return ('-');
-}
-
-int	ft_size(char *str)
-{
-	int	i;
-	int	a;
-
-	i = 0;
-	a = 0;
-	while (str[i] != '\0')
-	{
-		a++;
-		i++;
-	}
-	return (a);
-}
-
 int	ft_atoi(char *str)
 {
-	int		i;
-	int		size;
-	int		entero;
-	int		posicion;
-	char	signo;
+	int	i;
+	int	s;
+	int	numb;
 
-	posicion = 1;
-	entero = 0;
-	size = ft_size(str);
-	signo = ft_signo(str);
-	i = size;
-	while (i <= size && i != '\t')
+	i = 0;
+	s = 1;
+	numb = 0;
+	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
+		i++;
+	while (str[i] == '+' || str[i] == '-')
 	{
-		while (str[i] >= '0' && str[i] <= '9')
-		{
-			entero = entero + ((str[i] - 48) * posicion);
-			posicion = posicion * 10;
-			i--;
-		}
-		i--;
+		if (str[i] == '-')
+			s *= -1;
+		i++;
 	}
-	if (signo == '-')
-		return (entero = entero * -1);
-	else
-		return (entero);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		numb = (str[i] - '0') + (numb * 10);
+		i++;
+	}
+	return (numb * s);
 }
--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: jporta <jporta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 05:38:29 by jporta            #+#    #+#             */
-/*   Updated: 2021/07/07 18:44:51 by jporta           ###   ########.fr       */
+/*   Updated: 2021/07/08 15:22:21 by jporta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,29 +54,27 @@ int	ft_lentbase(char *base)
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int	unsigned_nbr;
-	int	lenbase;
+	unsigned int	lenbase;
+	unsigned int	nbr_unsigned;
 
 	if (check_error(base))
 	{
-		if (nbr < 0 && nbr != '\0')
+		if (nbr < 0)
 		{
 			write(1, "-", 1);
-			unsigned_nbr = (unsigned int)(nbr * -1);
+			nbr_unsigned = (unsigned int)(-1 * nbr);
+		}
+		else
+			nbr_unsigned = (unsigned int)nbr;
+		lenbase = ft_lentbase(base);
+		if (nbr_unsigned >= lenbase)
+		{
+			ft_putnbr_base(nbr_unsigned / lenbase, base);
+			ft_putnbr_base(nbr_unsigned % lenbase, base);
 		}
 		else
 		{
-			unsigned_nbr = (unsigned int)(nbr);
-			lenbase = ft_lentbase(base);
-			if (unsigned_nbr >= lenbase)
-			{
-				ft_putnbr_base(unsigned_nbr / lenbase, base);
-				ft_putnbr_base(unsigned_nbr % lenbase, base);
-			}
-			else
-			{
-				write(1, base + unsigned_nbr, 1);
-			}
+			write(1, base + nbr_unsigned, 1);
 		}
 	}
 }
